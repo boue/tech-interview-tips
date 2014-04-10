989efde70fdf94004926cae06a4d4055ef1434ef
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-10 14:33:20 -0700

    Migrations made for question, answer, user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :questions
+  has_many :answers
+
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
-  
+
   def self.create_from_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
--- db/migrate/20140410192144_add_slug_to_questions.rb
+++ db/migrate/20140410192144_add_slug_to_questions.rb
@@ -1,5 +1,6 @@
 class AddSlugToQuestions < ActiveRecord::Migration
   def change
     add_column :questions, :slug, :string
+    # slug is a friendly_id feature
   end
 end
