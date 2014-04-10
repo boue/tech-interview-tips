60811b867d09ee3f79a3481a065e082cbf556b81
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-10 15:33:42 -0700

    Model tests for Questions, few validations

--- spec/models/model_spec.rb
+++ spec/models/model_spec.rb
@@ -9,3 +9,13 @@ describe Answer do
     it { should have_many :kudos }
   end
 end
+
+describe Question do
+  context "associations" do
+    it { should belong_to :user }
+    it { should have_many :favorites }
+    it { should have_many :answers }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:content) }
+  end
+end
