fb38bc0ae779ac228e08c2e49ffff77ae60ef070
Mason O'Neal <masonwid@gmail.com>
2014-04-10 16:07:48 -0700

    Add correct models and migrations for Comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ActiveRecord::Base
   # basic self join lines 3-5 below
-  has_many :comment_child, class_name: "Comment",
-                          foreign_key: "comment_parent_id"
-  belongs_to :comment_parent, class_name: "Comment"
+  # has_many :comment_children, class_name: "Comment",
+  #                         foreign_key: "comment_parent_id"
+  # belongs_to :comment_parent, class_name: "Comment"
 
   # below makes polymorphic association
   belongs_to :commentable, polymorphic: true
--- db/migrate/20140410221202_create_comments.rb
+++ db/migrate/20140410221202_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string  :content
       # below is for self-join
-      t.references :comment_parent
+      # t.references :comment_parent
       # below is for polymorphic association
       t.references :commentable, polymorphic: true
       t.timestamps
