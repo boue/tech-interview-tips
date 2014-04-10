217a580a87629449028c27dd3eabe389d7507920
Mason O'Neal <masonwid@gmail.com>
2014-04-10 16:09:49 -0700

    Delete comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,4 @@
 class Comment < ActiveRecord::Base
-  # basic self join lines 3-5 below
-  # has_many :comment_children, class_name: "Comment",
-  #                         foreign_key: "comment_parent_id"
-  # belongs_to :comment_parent, class_name: "Comment"
-
-  # below makes polymorphic association
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
 end
--- db/migrate/20140410221202_create_comments.rb
+++ db/migrate/20140410221202_create_comments.rb
@@ -2,9 +2,6 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string  :content
-      # below is for self-join
-      # t.references :comment_parent
-      # below is for polymorphic association
       t.references :commentable, polymorphic: true
       t.timestamps
     end
