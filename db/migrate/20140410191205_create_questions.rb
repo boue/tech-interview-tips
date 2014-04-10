4cf7db28aecd3b6dc7db66987efac4518d107c3f
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-10 12:18:17 -0700

    Create class and migration for questions
      *migration for questions has t.integer :user_id
      *includes timestamp
      *create a model for it too

---
+++ db/migrate/20140410191205_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+      t.integer :views
+      t.timestamps
+    end
+  end
+end
