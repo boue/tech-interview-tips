3f00ab5e9ce8f2f0af0df549c9bd8de503e8ac95
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-09 21:00:29 -0700

    Initialized new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechInterviewTips::Application.config.secret_key_base = 'f99580bbd4fa7e4f00e5f4ad5b58b27e97ca5f8680cce6b4ddd0e6ff95ef8638a797004a3ab67a48fecc9bcd4852f17f2a549ea3ccae94f6a37b341bda9ed78c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TechInterviewTips::Application.config.session_store :cookie_store, key: '_tech-interview-tips_session'
