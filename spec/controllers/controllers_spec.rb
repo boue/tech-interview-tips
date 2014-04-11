467cbfc3748e275ac2efbdd099ba9f99df07bd78
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-10 20:55:04 -0700

    Test suite but one pending because of Omniauth sessions
      *We need to find a way to create a user and have a session
      started in order to be able to render the view apparently

--- spec/controllers/controllers_spec.rb
+++ spec/controllers/controllers_spec.rb
@@ -6,7 +6,7 @@ describe QuestionsController do
     let(:question) { FactoryGirl.create :question }
     it "is successful" do
       get :show, id: question.id
-      expect(response).to be_success
+      expect(response).to render_template(:show)
     end
 
     it "assigns @question to the Question found by id" do
@@ -15,12 +15,37 @@ describe QuestionsController do
     end
   end
 
+  context "#new" do
+    it "is successful" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns @question to the Question found by id" do
+      get :new
+      expect(assigns(:question)).to be_a_new Question
+    end
+  end
+
   context "#create" do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      session[:user_id] = @user.id
+    end
+
     it "if given valid parameters, it creates a new question" do
       expect {
         post(:create, question: FactoryGirl.attributes_for(:question))
       }.to change{Question.count}.by(1)
     end
-  end
+
+    xit "if given invalid parameters, it renders new view" do
+      expect {
+        get
+        post :create, :question => {}
+          # , question: {:title => new_title , :content => nil} )
+      }.to render_template :new
+      end
+    end
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,12 @@ FactoryGirl.define do
     title { Faker::Lorem.word }
     content { Faker::Lorem.sentence }
   end
+
+  factory :user do
+    provider { Faker::Lorem.word }
+    uid { Faker::Lorem.word }
+    name { Faker::Lorem.word }
+    email { Faker::Internet.email }
+  end
+
 end
