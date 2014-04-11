5d19abaf464417c7e419a581d37e266e6c902f24
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-10 17:25:51 -0700

    Added friendly id code to question model.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,14 @@ class Question < ActiveRecord::Base
   has_many :answers
   belongs_to :user
   has_many :favorites, as: :favoritable
+
+  default_scope -> { order('created_at DESC') }
+  
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :history]
+  validates_presence_of :title, :slug, :content
+
+  def should_generate_new_friendly_id?
+    headline_changed?
+  end
 end
