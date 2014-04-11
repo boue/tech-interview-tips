2f901ec8235ab1dda5e01096ac18bd8716533029
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-10 21:24:39 -0700

    Adds index, edit, update, destroy methods
      * Controller now has the above methods
      * View has edit and index pages
      * Show has links to edit/delete question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,9 @@
 class QuestionsController < ApplicationController
 
+  def index
+    @questions = Question.all
+  end
+
   def show
     @question = Question.find params[:id]
   end
@@ -9,12 +13,31 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new (question_params)
+    @question = Question.new question_params
     if @question.save
       redirect_to question_path(@question)
     else
       render :new
     end
+  end
+
+  def edit
+    @question = Question.find params[:id]
+  end
+
+  def update
+    @question = Question.find params[:id]
+    if @question.update_attributes question_params
+      redirect_to question_path(@question)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    question = Question.find params[:id]
+    question.destroy
+    redirect_to root_path
   end
 
   private
