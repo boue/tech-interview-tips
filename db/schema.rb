8897203e8fdbc9e4912b6a98909638b5dc6ccd9a
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-10 17:19:07 -0700

    Added slug index to questions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140410215042) do
+ActiveRecord::Schema.define(version: 20140411001636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,19 @@ ActiveRecord::Schema.define(version: 20140410215042) do
     t.datetime "updated_at"
   end
 
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "kudos", force: true do |t|
     t.integer  "user_id"
     t.integer  "kudosible_id"
@@ -56,6 +69,8 @@ ActiveRecord::Schema.define(version: 20140410215042) do
     t.datetime "updated_at"
     t.string   "slug"
   end
+
+  add_index "questions", ["slug"], name: "index_questions_on_slug", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.string   "provider"
