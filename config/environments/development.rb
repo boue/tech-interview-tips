27c76fa603b0349302cd8da39ef0dbbda2c7830a
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-10 23:44:01 -0700

    Changed location of require dotenv

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
 TechInterviewTips::Application.configure do
-  require "dotenv"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
