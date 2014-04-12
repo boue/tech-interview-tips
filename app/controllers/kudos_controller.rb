61ae5e72e2d4c5ed960010083a23fcc80da7f9ed
Drew Parker <apprentice@dbc05.local>
2014-04-12 16:26:16 -0700

    Adds private method to KudosController so that user can only give kudos once

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,12 +1,27 @@
 class KudosController < ApplicationController
 
   def create
-    kudo = Kudo.new
-    question = Question.find(params[:question_id])
-    kudo.user_id = current_user.id
-    question.kudos << kudo
-    if kudo.save
-      redirect_to question_path(question)
+    @kudo = Kudo.new
+    @question = Question.find(params[:question_id])
+    if has_not_given_kudos?
+      @kudo.user_id = current_user.id
+      @question.kudos << @kudo
+      @kudo.save
     end
+    redirect_to question_path(@question)
   end
+
+  private
+  def has_not_given_kudos?
+    bool = false
+    @question.kudos.each do |kudo|
+      if @kudo.user_id == current_user.id
+        bool = true
+      else
+        bool = false
+      end
+    end
+    bool
+  end
+
 end
