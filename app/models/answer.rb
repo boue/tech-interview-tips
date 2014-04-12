3f8748f0663af57f4ffa73cf90e25e6efbe1c868
Drew Parker <apprentice@dbc05.local>
2014-04-12 12:15:43 -0700

    adds dependent destroy to Question and Answer models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,7 @@ class Answer < ActiveRecord::Base
   belongs_to :category
   has_many :favorites, as: :favoritable
   has_many :kudos, as: :kudosible
-  has_many :comments, as: :commentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :content, presence: true
 
