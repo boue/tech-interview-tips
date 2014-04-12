8786fccd2a0176981910605d936ed88c7a0f2a11
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-12 09:22:27 -0700

    Played with some styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411223456) do
+ActiveRecord::Schema.define(version: 20140412004639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 20140411223456) do
   create_table "questions", force: true do |t|
     t.string   "title"
     t.text     "content"
-    t.integer  "category_id"
     t.integer  "user_id"
     t.integer  "views"
     t.datetime "created_at"
