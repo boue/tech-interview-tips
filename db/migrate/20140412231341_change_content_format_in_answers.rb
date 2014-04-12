784372fcd673d46b618725a2e22319854910a2c4
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-12 16:25:06 -0700

    Fixed migration

--- db/migrate/20140412231341_change_content_format_in_answers.rb
+++ db/migrate/20140412231341_change_content_format_in_answers.rb
@@ -1,4 +1,4 @@
-class ChangeAnswerContentFormatInQuestions < ActiveRecord::Migration
+class ChangeContentFormatInAnswers < ActiveRecord::Migration
   def change
     change_column :answers, :content, :text
   end
