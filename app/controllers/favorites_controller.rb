9419c43af9f481b1be8c17136e0b01d2c26d5e67
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-12 18:40:09 -0700

    Added saves label to index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
 
   def create
-    @favorite = Favorite.new
-    @question = Question.find(params[:question_id])
-    if @question.favorites.where(user_id: current_user.id).count == 0
-      @favorite.user_id = current_user.id
-      @question.favorites << @favorite
-      @favorite.save
+    favorite = Favorite.new
+    question = Question.find(params[:question_id])
+    if question.favorites.where(user_id: current_user.id).count == 0
+      favorite.user_id = current_user.id
+      question.favorites << favorite
+      favorite.save
     end
-    redirect_to question_path(@question)
+    redirect_to question_path(question)
   end
 end
