1a51e5c3425aec512a4b8e431e19354153cec0cd
Drew Parker <apprentice@dbc05.local>
2014-04-12 18:21:59 -0700

    User can add kudo to answers, but only once

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -3,10 +3,20 @@ class KudosController < ApplicationController
   def create
     @kudo = Kudo.new
     @question = Question.find(params[:question_id])
-    if @question.kudos.where(user_id: current_user.id).count == 0
-      @kudo.user_id = current_user.id
-      @question.kudos << @kudo
-      @kudo.save
+    if params[:answer_id]
+      @answer = Answer.find(params[:answer_id])
+      if @answer.kudos.where(user_id: current_user.id).count == 0
+        @kudo.user_id = current_user.id
+        @answer.kudos << @kudo
+        @kudo.save
+      end
+    else
+      @question = Question.find(params[:question_id])
+      if @question.kudos.where(user_id: current_user.id).count == 0
+        @kudo.user_id = current_user.id
+        @question.kudos << @kudo
+        @kudo.save
+      end
     end
     redirect_to question_path(@question)
   end
