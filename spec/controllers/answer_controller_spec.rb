43cb2ae599533c69145431237c6309c619219b73
Mason O'Neal <masonwid@gmail.com>
2014-04-12 18:19:34 -0700

    Add Answer controller tests

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -1,51 +1,53 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe AnswersController do
+describe AnswersController do
 
-#   context "#show" do
-#     let(:question) { FactoryGirl.create :question }
-#     it "is successful" do
-#       get :show, id: question.id
-#       expect(response).to render_template(:show)
-#     end
+  context "#show" do
+    let(:answer) { FactoryGirl.create :answer }
+    it "is successful" do
+      get :show, id: answer.id
+      expect(response).to render_template(:show)
+    end
 
-#     it "assigns @question to the Question found by id" do
-#       get :show, id: question.id
-#       expect(assigns(:question)).to eq question
-#     end
-#   end
+    it "assigns @answer to the answer found by id" do
+      get :show, id: answer.id
+      expect(assigns(:answer)).to eq answer
+    end
+  end
 
-#   context "#new" do
-#     it "is successful" do
-#       get :new
-#       expect(response).to render_template(:new)
-#     end
+  # context "#new" do
+  #   problem due to no new view, instead is a partial _new
+  #   it "is successful" do
+  #     get :new
+  #     expect(response).to render_template(:new)
+  #   end
 
-#     it "assigns @question to the Question found by id" do
-#       get :new
-#       expect(assigns(:question)).to be_a_new Question
-#     end
-#   end
+  #   it "assigns @answer to the answer found by id" do
+  #     get :new
+  #     expect(assigns(:answer)).to be_a_new Answer
+  #   end
+  # end
 
-#   context "#create" do
-#     before :each do
-#       @user = FactoryGirl.create(:user)
-#       session[:user_id] = @user.id
-#     end
+  context "#create" do
+  #   before :each do
+  #     @user = FactoryGirl.create(:user)
+  #     session[:user_id] = @user.id
+  #   end
 
-#     it "if given valid parameters, it creates a new question" do
-#       expect {
-#         post(:create, question: FactoryGirl.attributes_for(:question))
-#       }.to change{Question.count}.by(1)
-#     end
+  # needs work
+    it "if given valid parameters, it creates a new question" do
+      expect {
+        post(:create, answer: FactoryGirl.attributes_for(:answer))
+      }.to change{Answer.count}.by(1)
+    end
 
-#     xit "if given invalid parameters, it renders new view" do
-#       expect {
-#         get
-#         post :create, :question => {}
-#           # , question: {:title => new_title , :content => nil} )
-#       }.to render_template :new
-#       end
-#     end
+  #   xit "if given invalid parameters, it renders new view" do
+  #     expect {
+  #       get
+  #       post :create, :question => {}
+  #         # , question: {:title => new_title , :content => nil} )
+  #     }.to render_template :new
+  #     end
+  end
 
-# end
+end
