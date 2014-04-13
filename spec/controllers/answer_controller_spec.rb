8b39ac0e097c3590e6fe6567c6ccf94ebd23f772
Mason O'Neal <masonwid@gmail.com>
2014-04-13 15:28:11 -0700

    Clean up spec code & Rename controller_spec to question_controller_spec

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -16,26 +16,12 @@ describe AnswersController do
     end
   end
 
-  # context "#new" do
-  #   # problem due to no new view, instead is a partial _new
-  #   it "is successful" do
-  #     get :new
-  #     expect(response).to render_template(partial: :_new)
-  #   end
-
-    # it "assigns @answer to the answer found by id" do
-    #   get :new
-    #   expect(assigns(:answer)).to be_a_new Answer
-    # end
-  # end
-
   context "#create" do
     before :each do
       @user = FactoryGirl.create :user
       @question = FactoryGirl.create :question
     end
 
-  # needs work
     it "if given valid parameters, it creates a new answer" do
       expect {
         AnswersController.any_instance.stub(:current_user).and_return(@user)
@@ -43,14 +29,6 @@ describe AnswersController do
           )
       }.to change{Answer.count}.by(1)
     end
-
-  #   xit "if given invalid parameters, it renders new view" do
-  #     expect {
-  #       get
-  #       post :create, :question => {}
-  #         # , question: {:title => new_title , :content => nil} )
-  #     }.to render_template :new
-  #     end
   end
 
 end
