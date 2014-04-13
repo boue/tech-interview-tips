337ac8384858f179044c123e20c3855fb4328318
Drew Parker <apprentice@dbc05.local>
2014-04-12 18:02:23 -0700

    adds check so that user can only kudo a question once

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -3,25 +3,12 @@ class KudosController < ApplicationController
   def create
     @kudo = Kudo.new
     @question = Question.find(params[:question_id])
-    if has_not_given_kudos?
+    if @question.kudos.where(user_id: current_user.id).count == 0
       @kudo.user_id = current_user.id
       @question.kudos << @kudo
       @kudo.save
     end
     redirect_to question_path(@question)
-  end
-
-  private
-  def has_not_given_kudos?
-    bool = false
-    @question.kudos.each do |kudo|
-      if @kudo.user_id == current_user.id
-        bool = true
-      else
-        bool = false
-      end
-    end
-    bool
   end
 
 end
