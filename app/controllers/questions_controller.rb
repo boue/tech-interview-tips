d4c2a8ce593114579fea874e6f4727870aa35908
jackiejohnston <jackiejohnston5@gmail.com>
2014-04-15 11:14:40 -0700

    Removed the Pry line

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,7 +33,7 @@ class QuestionsController < ApplicationController
     if @question.save
       redirect_to question_path(@question)
     else
-      require "pry"; binding.pry
+      # require "pry"; binding.pry
       render :new
     end
   end
