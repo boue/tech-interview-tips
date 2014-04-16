5c208c87efbea6ae9e63361ec84486d46650759f
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-16 16:26:29 -0700

    Major refactor for kudos
      *have one format_json instead of two
      *checks if it is an object that is kudosible time
      *assigns kudosible to a user if not yet assigned

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,25 +1,20 @@
 class KudosController < ApplicationController
-
   def create
-    @kudo = Kudo.new
-    if params[:answer_id]
-      @answer = Answer.find(params[:answer_id])
-      Kudo.assign(@answer, current_user, @kudo)
-      respond_to do |format|
-        format.json {render json: {id: @answer.id, kudos_count: @answer.kudos.count}}
-        format.html { redirect_to question_path(@answer.question) }
+    kudo = Kudo.new
+    if params[:kudosible_type] && params[:kudosible_id]
+      kudosible = params[:kudosible_type].capitalize.constantize.find(params[:kudosible_id])
+      unless kudosible.kudos.find_by(user_id: current_user.id)
+        kudo.user_id = current_user.id
+        kudosible.kudos << kudo
+        kudo.save
       end
-
-    elsif params[:question_id]
-      @question = Question.find(params[:question_id])
-      Kudo.assign(@question, current_user, @kudo)
+      question = kudosible.respond_to?(:question) ? kudosible.question : kudosible
       respond_to do |format|
-        format.json {render json: {kudos_count: @question.kudos.count}}
-        format.html { redirect_to question_path(@question) }
+        format.json {render json: {id: kudosible.id, kudos_count: kudosible.kudos.count}}
+        format.html { redirect_to question_path(question) }
       end
     else
       redirect_to root_path, flash: { notice: "Kudos create was unsuccessful" }
     end
   end
-
 end
