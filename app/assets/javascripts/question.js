b82471d045a7471b6d718465655228bcc6b7aa46
Germain Boue & Drew Parker <pair+boue.drewcp87@devbootcamp.com>
2014-04-16 17:41:53 -0700

    Changes made to answers/questions in order to take into account changes to interface
      *kudosible_id

--- app/assets/javascripts/question.js
+++ app/assets/javascripts/question.js
@@ -9,13 +9,14 @@ $(document).ready(function(){
   });
 
   $('#thumbs-up-link').on("ajax:success", function (e, data, status, xhr) {
+    console.log(data)
     $('#thumbs-up-count').html(data.kudos_count)
     $('#thumbs-up-link').addClass('grey-out')
     $('#thumbs-up-link').addClass('inactive')
   });
 
   $(".answer-thumbs-up").on("ajax:success", function (e, data, status, xhr) {
-    // alert(data.kudos_count)
+    console.log(data)
     $('#'+data.id+' .answer-vote').html(data.kudos_count)
     $('#'+data.id+' .answer-thumbs-up').addClass('grey-out')
     $('#'+data.id+' .answer-thumbs-up').addClass('inactive')
